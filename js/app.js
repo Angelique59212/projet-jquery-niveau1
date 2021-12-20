2e1100ddcb37569b222de1635efcc088e0b80bb1
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-12-20 19:18:27 +0100

    projet niveau1

---
+++ js/app.js
@@ -0,0 +1,12 @@
+$("#myId").html("Hello World");
+$("a").attr("href", "https://learn.jquery.com/using-jquery-core/attributes/");
+
+let monDiv = $("#div").children("div").next(".deuxiemediv").text("").css( {
+    "width" : "100px",
+    "height": "200px",
+});
+
+
+
+
+
