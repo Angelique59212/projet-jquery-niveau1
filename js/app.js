158dfea177a3d03ee4145ff1da533abeece71dd7
Angelique59212 <92031713+Angelique59212@users.noreply.github.com>
2021-12-21 09:47:02 +0100

    projet niveau1

--- js/app.js
+++ js/app.js
@@ -1,12 +1,17 @@
 $("#myId").html("Hello World");
-$("a").attr("href", "https://learn.jquery.com/using-jquery-core/attributes/");
 
-let monDiv = $("#div").children("div").next(".deuxiemediv").text("").css( {
+let a = $("a");
+
+a.html("mon lien").attr("href", "https://learn.jquery.com/using-jquery-core/attributes/");
+
+let monDiv= $("#monDiv");
+    monDiv.css( {
     "width" : "100px",
     "height": "200px",
+    "border" : "1px solid black"
 });
-
-
+    $("#monDiv div").eq(1).text("Mon deuxieme div");
+alert($("#monDiv div").eq(1).index())
 
 
 
